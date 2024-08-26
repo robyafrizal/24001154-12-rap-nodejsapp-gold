8c731aee72da0a5c597d2fa6bfdf25abb22b48f9
robyafrizal <robyafrizal86@gmail.com>
2024-08-27 00:11:14 +0700

    Deploy ci/cd - gcp v.1

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
+// import dotenv from 'dotenv'
+// dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
