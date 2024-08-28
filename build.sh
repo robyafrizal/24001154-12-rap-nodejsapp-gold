dd2acf70beff3c6add1df5863b7cd0599e95551b
robyafrizal <robyafrizal86@gmail.com>
2024-08-28 23:34:41 +0700

    Fir error again v2

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
-git pull
-pm2 delete main
-pm2 start main.js
-
-# git pull origin main
-# pm2 delete 24001154-12-rap-nodejsapp-gold
-# pm2 start main.js -n 24001154-12-rap-nodejsapp-gold
+git pull origin main
+pm2 delete 24001154-12-rap-nodejsapp-gold
+pm2 start main.js -n 24001154-12-rap-nodejsapp-gold
